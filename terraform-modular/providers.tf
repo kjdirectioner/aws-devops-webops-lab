384671bb32a5e5235a824d201d0d0ee2153426be
kj directioner <kjdirectioner@kjs-Mac-mini.local>
2026-08-06 21:31:24 +0530

    configured s3 remote state and dynamodb state locking

--- terraform-modular/providers.tf
+++ terraform-modular/providers.tf
@@ -5,6 +5,14 @@ terraform {
       version = "~> 5.0"
     }
   }
+  backend "s3" {
+    bucket         = "tf-state-aws-devops-lab"
+    key            = "global/s3/terraform.tfstate"
+    region         = "eu-north-1"
+    dynamodb_table = "terraform-state-locks"
+    encrypt        = true
+  }
+
 }
 
 provider "aws" {
